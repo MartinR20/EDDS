2e04303c96b96d6f7baf79151727791726c7a874
Martin Reichholf <reichholf.martin@gmail.com>
2024-12-22 22:45:15 +0100

    added data

--- run.sh
+++ run.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-SEED=73308559
-#SEED=83638859
-#SEED=63381133
-#SEED=61451749
-#SEED=88666223
-#SEED=45755797
-#SEED=34521869
-#SEED=93643757
+SEED=$1
 
 mkdir $SEED
 
@@ -14,8 +7,8 @@ cd $SEED
 
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_netflix4.csv    -s ../data/sigma_netflix4.csv    > netflix4.csv      &
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_netflix8.csv    -s ../data/sigma_netflix8.csv    > netflix8.csv      &
-../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_netflix16.csv   -s ../data/sigma_netflix16.csv   > netflix16.csv     &
-../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_netflix32.csv   -s ../data/sigma_netflix32.csv   > netflix32.csv     &
+#../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_netflix16.csv   -s ../data/sigma_netflix16.csv   > netflix16.csv     &
+#../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_netflix32.csv   -s ../data/sigma_netflix32.csv   > netflix32.csv     &
 
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_goodreads4.csv  -s ../data/sigma_goodreads4.csv  > goodreads4.csv    & 
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_goodreads8.csv  -s ../data/sigma_goodreads8.csv  > goodreads8.csv    & 
@@ -26,3 +19,4 @@ cd $SEED
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_jester8.csv     -s ../data/sigma_jester8.csv     > jester8.csv       &  
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_jester16.csv    -s ../data/sigma_jester16.csv    > jester16.csv      &
 ../bin/mcts -e $SEED -t 15 -n 25 -m ../data/mu_jester32.csv    -s ../data/sigma_jester32.csv    > jester32.csv      & 
+
